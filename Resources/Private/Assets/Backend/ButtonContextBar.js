0d97001bcf53be27b02f4295876fce995db45dab
Jon Uhlmann <me@jonnitto.ch>
2018-05-25 21:18:47 +0200

    :bug: Fix Button in context bar

--- Resources/Private/Assets/Backend/ButtonContextBar.js
+++ Resources/Private/Assets/Backend/ButtonContextBar.js
@@ -1,7 +1,14 @@
 const IS_NEW_UI = !!(window.name == "neos-content-main");
 const SITE_DOC = window.document;
 const NEOS_DOC = IS_NEW_UI ? window.parent.document : SITE_DOC;
-const NAMESPACE = "__ggbuttoncontextbar";
+const NAMESPACE = "__carbonbuttoncontextbar";
+const ICONS_BASE = `style="fill:#fff" class="neos-svg-inline--fa neos-fa-image fa-w-18" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"`;
+const ICONS = {
+    images: `<svg ${ICONS_BASE} viewBox="0 0 576 512"><path d="M480 416v16c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V176c0-26.51 21.49-48 48-48h16v208c0 44.112 35.888 80 80 80h336zm96-80V80c0-26.51-21.49-48-48-48H144c-26.51 0-48 21.49-48 48v256c0 26.51 21.49 48 48 48h384c26.51 0 48-21.49 48-48zM256 128c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-96 144l55.515-55.515c4.686-4.686 12.284-4.686 16.971 0L272 256l135.515-135.515c4.686-4.686 12.284-4.686 16.971 0L512 208v112H160v-48z"/></svg>`,
+    ellipsis: `<svg ${ICONS_BASE} viewBox="0 0 512 512"><path d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z"/></svg>`,
+    arrows: `<svg ${ICONS_BASE} viewBox="0 0 512 512"><path d="M377.941 169.941V216H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.568 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296h243.882v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.568 0-33.941l-86.059-86.059c-15.119-15.12-40.971-4.412-40.971 16.97z"/></svg>`,
+    camera: `<svg ${ICONS_BASE} viewBox="0 0 512 512"><path d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z"/></svg>`
+};
 
 let element = null;
 let settings = null;
@@ -90,7 +97,8 @@ function addButton() {
             button.blur();
             triggerButton(button);
         };
-        button.innerHTML = `&#x${settings.icon};`;
+        button.innerHTML =
+            settings.icon in ICONS ? ICONS[settings.icon] : settings.icon;
         element.insertBefore(button, element.firstChild);
 
         if (NEOS_DOC[NAMESPACE][settings.toggleClass]) {
@@ -111,7 +119,9 @@ function addButton() {
 function buttonCallback(button, callback) {
     button = button
         ? button
-        : element ? element.querySelector(`.${settings.className}`) : false;
+        : element
+            ? element.querySelector(`.${settings.className}`)
+            : false;
     if (button) {
         callback(button);
     }
