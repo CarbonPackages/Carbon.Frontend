94837d08afe9b34f84f62132f366085a1f50069c
Jon Uhlmann <me@jonnitto.ch>
2018-07-23 13:33:50 +0200

    :sparkles: Extend isLive

--- Resources/Private/Assets/Scripts/isLive.js
+++ Resources/Private/Assets/Scripts/isLive.js
@@ -1,3 +1,5 @@
-const isLive = !document.body.classList.contains("neos-backend");
+// The html tag need the class "-live" in the live context
+
+const isLive = document.documentElement.classList.contains("-live");
 
 export default isLive;
