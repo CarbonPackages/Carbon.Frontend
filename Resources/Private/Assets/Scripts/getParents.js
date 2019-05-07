6a6e10574f0f8670e1519bf2ea72e765dd8a40c5
Jon Uhlmann <me@jonnitto.ch>
2019-05-07 16:22:58 +0200

    :sparkles: Add getParents function

---
+++ Resources/Private/Assets/Scripts/getParents.js
@@ -0,0 +1,27 @@
+import checkMatch from './checkMatch';
+
+export default function(element, selector, topElement) {
+    let parents = [];
+
+    // If the element is not set or not a node return empty array
+    if (!element || element.nodeType != 1) {
+        return parents;
+    }
+
+    // If the top element is not set or not a node set it to document
+    if (!topElement || topElement.nodeType != 1) {
+        topElement = document;
+    }
+    let parentElement = element.parentNode;
+    while (parentElement !== topElement) {
+        if (checkMatch(parentElement, selector)) {
+            parents.push(parentElement);
+        }
+        parentElement = parentElement.parentNode;
+    }
+    if (checkMatch(parentElement, selector)) {
+        parents.push(parentElement);
+    }
+
+    return parents;
+}
